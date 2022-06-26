d759655980034e23f6f6853b2c26af948effd70e
Dulshan Perera <dulshanperera55@gmail.com>
2022-06-26 23:06:50 +0530

    Integrated cart and inventory functions

--- Frontend/src/api/promotion.api.js
+++ Frontend/src/api/promotion.api.js
@@ -1,4 +1,7 @@
-import axios from 'axios.config.js'
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:3000/';
+axios.defaults.headers.common['authentication'] = localStorage.getItem('authentication');
 
 export const addPromotion = (promoObj) => axios.post(`/promotions`, promoObj);
 export const fetchPromotions = () => axios.get(`/promotions`);
---
+++ Frontend/src/pages/AddItem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { addItem } from "../api/item.api";
+
+export default AddItem = () =>{
+    const [item, setItem] = useState({
+        name:"",
+        price:""
+    })
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        addItem(item).then((res) =>{
+            console.log(res)
+        }).catch((error) =>{
+            console.log(error)
+        })
+    }
+
+    return (
+        <div>
+            <h2>Add Item</h2>
+            <form>
+                <label>Enter Item name</label>
+                <input type="text" name='name' value={item.name} onChange={(e) => setItem({...item, name:e.target.value})}/> <br/>
+                <label>Enter Item Price</label>
+                <input type="text" name="price" value={item.price} onChange={(e) => setItem({...item, price:e.target.value})}/> <br/>
+                
+                <button type='submit' onClick = {handleSubmit}>Submit</button>
+            </form>
+        </div>
+    )
+}
